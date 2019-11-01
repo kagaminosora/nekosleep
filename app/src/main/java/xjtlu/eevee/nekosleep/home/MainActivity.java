f7cb528d6aa410e684650869066417493eaca4b3
Luhan <731259623@qq.com>
2019-11-01 14:16:03 +0800

    migrate to Androidx

--- app/src/main/java/xjtlu/eevee/nekosleep/home/MainActivity.java
+++ app/src/main/java/xjtlu/eevee/nekosleep/home/MainActivity.java
@@ -1,6 +1,6 @@
 package xjtlu.eevee.nekosleep.home;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import xjtlu.eevee.nekosleep.R;
