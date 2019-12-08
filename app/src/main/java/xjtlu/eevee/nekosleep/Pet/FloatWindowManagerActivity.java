7ddc301a9fe3ebe1e42b052103748f7d3cb4f943
Anslote <879016240@qq.com>
2019-12-08 21:11:09 +0800

    interface for launching pet

--- app/src/main/java/xjtlu/eevee/nekosleep/Pet/FloatWindowManagerActivity.java
+++ app/src/main/java/xjtlu/eevee/nekosleep/Pet/FloatWindowManagerActivity.java
@@ -3,7 +3,6 @@ package xjtlu.eevee.nekosleep.Pet;
 import android.Manifest;
 import android.app.Service;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
@@ -11,14 +10,13 @@ import android.view.View;
 import android.widget.Button;
 import android.provider.Settings;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import xjtlu.eevee.nekosleep.R;
 
-public class MainActivity extends AppCompatActivity {
+public class FloatWindowManagerActivity extends AppCompatActivity {
 
     int OVERLAY_PERMISSION_REQ_CODE = 0;
     boolean petStarted = false;
@@ -61,21 +59,17 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-//    private void checkAndRequirePermission () {
-//        if (Build.VERSION.SDK_INT >= 23) {
-//            if (ContextCompat.checkSelfPermission(this, Manifest.permission.SYSTEM_ALERT_WINDOW) != PackageManager.PERMISSION_GRANTED) {
-//                ActivityCompat.requestPermissions(this, new String[]{ Manifest.permission.SYSTEM_ALERT_WINDOW }, 0);
-//                System.out.println("Success?");
-//            }
-//        }
-//    }
-//
-//    @Override
-//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-//        if (requestCode == 0) {
-//            System.out.println("Success!");
-//        }
-//    }
+    public void launchPet () {
+        if (!petStarted) {
+            petIntent = new Intent(getApplicationContext(), FloatWindowManagerService.class);
+            startService(petIntent);
+            petStarted = true;
+            System.out.println("Service started.");
+        } else {
+            stopService(petIntent);
+            petStarted = false;
+            System.out.println("Service stopped.");
+        }
+    }
 
 }
