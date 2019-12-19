c67a2eb84e76a07dfe8235cb63444eda68d24411
RX59G <Shuai.Chen16@student.xjtlu.edu.cn>
2019-12-20 06:47:03 +0800

    try to monitor screen lock and state of mainactivity

--- app/src/main/java/xjtlu/eevee/nekosleep/home/SleepTime.java
+++ app/src/main/java/xjtlu/eevee/nekosleep/home/SleepTime.java
@@ -13,11 +13,36 @@ public class SleepTime {
         int sleephour = hour(sleeptime);
         int sleepmin = min(sleeptime);
         int wakehour = hour(waketime);
-        int wakemin = hour(waketime);
-        if (sleephour >= wakehour){
+        int wakemin = min(waketime);
+        if (sleephour > wakehour){
             return (12-sleephour-1)*60+(60-sleepmin)+wakehour*60+wakemin;
         }else{
             return (wakehour-sleephour)*60+(wakemin-sleepmin);
         }
+    }
+
+    public boolean within1h(String time_act, String time_set){
+        if (hour(time_set)==23){
+            if (hour(time_act)==0){
+                if(min(time_set)>=min(time_act)){return true;}
+                else return false;
+            }else{
+                if(sleepLast(time_act,time_set)<=60){return true;}
+                else return false;
+            }
+        }
+        else if (hour(time_set)==0){
+            if (hour(time_act)==23){
+                if(min(time_act)>=min(time_set)){
+                    return true;
+                }else return false;
+            }else{
+                if(sleepLast(time_act,time_set)<=60){return true;}
+                else return false;
+            }
+        }
+        else if(sleepLast(time_act,time_set)<=60||sleepLast(time_set,time_set)<=60){
+            return true;
+        }else return false;
     }
 }
